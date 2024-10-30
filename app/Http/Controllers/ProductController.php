910ed2f37eb8a82082ab8ec2725fd27558430bd0
Addd12 <addd.github@gmail.com>
2024-10-30 23:54:15 +0100

    added index page for showing products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(Request $request){
-    
+        $products = Product::all();
+        return view('product.index', compact('products'));
     }
 }
